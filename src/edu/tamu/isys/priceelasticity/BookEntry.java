18c829955d8fc8f54c7708498bde25b28b3fc7a6
Vijaylakshmi <vrana18@gmail.com>
2015-12-01 10:51:55 -0600

    Modified Book classes

--- src/edu/tamu/isys/priceelasticity/BookEntry.java
+++ src/edu/tamu/isys/priceelasticity/BookEntry.java
@@ -3,71 +3,82 @@ package edu.tamu.isys.priceelasticity;
 import java.text.ParseException;
 
 public class BookEntry {
-	
-	private String ISBN= "";
-	private String title= "";
-	private String publisher= "";
-	private String publishingDate= "";
-	private String price= "";
+
+	private String dataMonth = "";
+	private String bookID = "";
+	private String bookName = "";
+	private String price = "";
+	private String quantity = "";
+	private String bookType = "";
+	private String origPubPrice = "";
+	private String growthRate = "";
+
 	private String errorMessage = "";
-	
-	public BookEntry (String rawData) throws ParseException
-	{
-		try
-		{
+
+	public BookEntry(String rawData) throws ParseException {
+		try {
 			String[] parts = rawData.split("::");
-			this.ISBN = parts[0].trim();
-			this.title=parts[1].trim();
-			this.publisher=parts[3].trim();
-			this.publishingDate=parts[4].trim();
-			this.price=parts[5].trim();
-		}
-		catch (Exception e)
-		{
-			errorMessage = e.getStackTrace().toString()+":"+rawData;
+			this.dataMonth = parts[0].trim();
+			this.bookID = parts[1].trim();
+			this.bookName = parts[2].trim();
+			this.price = parts[3].trim();
+			this.quantity = parts[4].trim();
+			this.bookType = parts[5].trim();
+			this.origPubPrice = parts[6].trim();
+			this.growthRate = parts[7].trim();
+		} catch (Exception e) {
+			errorMessage = e.getStackTrace().toString() + ":" + rawData;
 			System.out.println(errorMessage);
 		}
 	}
-	
-	/* Method to return Book ISBN */
-	public String getISBN()
-	{
-		return ISBN;
+
+	/* Method to return Book DataMonth */
+	public String getDataMonth() {
+		return dataMonth;
 	}
-	
-	/* Method to return Book Title */
-	public String getTitle()
-	{
-		return title;
+
+	/* Method to return Book ID */
+	public String getBookID() {
+		return bookID;
 	}
-	
-	/* Method to return Book Publisher*/
-	public String getPublisher()
-	{
-		return publisher;
+
+	/* Method to return Book Name */
+	public String getBookName() {
+		return bookName;
 	}
-	
-	/* Method to return Book Publishing Date */
-	public String getPublishingDate()
-	{
-		return publishingDate;
-	}
-	
+
 	/* Method to return Book Price */
-	public String getPrice()
-	{
+	public String getPrice() {
 		return price;
 	}
-	
+
+	/* Method to return Book Quantity */
+	public String getQuantity() {
+		return quantity;
+	}
+
+	/* Method to return Book Type */
+	public String getBookType() {
+		return bookType;
+	}
+
+	/* Method to return Book Original Publisher Price */
+	public String getOrigPubPrice() {
+		return origPubPrice;
+	}
+
+	/* Method to return Book Growth Rate */
+	public String getGrowthRate() {
+		return growthRate;
+	}
+
 	/* Method to return Error Message as string */
-	public String getError()
-	{
+	public String getError() {
 		return errorMessage;
 	}
-	
+
 	/* Method to return the status of Error Message Code */
-	public boolean hasError()
-	{
+	public boolean hasError() {
 		return !errorMessage.isEmpty();
 	}
 }
--- src/edu/tamu/isys/priceelasticity/TextBookEntry.java
+++ src/edu/tamu/isys/priceelasticity/TextBookEntry.java
@@ -3,14 +3,13 @@ package edu.tamu.isys.priceelasticity;
 import java.text.ParseException;
 
 public class TextBookEntry extends BookEntry {
-	private String edition="";
-	
+	private String edition = "";
+
 	public TextBookEntry(String rawData) throws ParseException {
 		super(rawData);
 	}
-	
-	public String getEdition()
-	{
+
+	public String getEdition() {
 		return edition;
 	}
 }
